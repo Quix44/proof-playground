233b47f565eceac66ca31db4023ace801b0c6fc8
Quix <quixcodes@gmail.com>
2024-03-17 15:25:52 +0100

    Solves a major bug

--- proof/src/circuit.rs
+++ proof/src/circuit.rs
@@ -7,7 +7,7 @@ use num_traits::Num;
 use rand::rngs::OsRng;
 use sha2::{Digest, Sha256};
 use std::convert::TryInto;
-// Insane!!!
+// Insane!!!!
 
 // Doco
 const BLS12_381_SCALAR_FIELD_ORDER: &str =
